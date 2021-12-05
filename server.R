f337570c9492308db33a56db284890a2828cee7b
Tigran Melkonian <tigmelkonian@gmail.com>
2021-12-05 11:14:29 -0800

    Initial commit

---
+++ server.R
@@ -0,0 +1,207 @@
+source("source_wikipedia_data.R")
+
+
+server <- function(input, output, session) {
+  
+  #################################################################################################
+  #                                       DATA OUTPUT ELEMENTS                                   #
+  ###############################################################################################
+  
+  
+  #############################
+  # page view raw data table #
+  ###########################
+  
+  output$wikipedia_df_raw <- renderDataTable({
+    # initialize updated data for data table output
+    wikipedia_raw_df <- session_data_input() %>%
+      select(
+        "fullname",
+        "week_start",
+        "weekly_pageviews"
+      ) %>%
+      arrange(desc(fullname))
+    # set custome features for data table output
+    datatable(wikipedia_raw_df,
+      options = list(
+        paging = T, scrollX = F, searching = T, bInfo = T,
+        autoWidth = F,
+        class = "cell-border hover compact",
+        pageLength = 10,
+        initComplete = JS(
+          "function(settings, json) {",
+          "$(this.api().table().header()).css({'background-color': '#000', 'color': '#fff'});",
+          "}"
+        )
+      ),
+      escape = F
+    )
+  })
+
+  #######################################
+  # page view summary stats data table #
+  #####################################
+  
+  output$summarystats <- renderDataTable({
+    # initialize updated data for data table output
+    wikipedia_summary_df <- session_data_input() %>%
+      select(
+        fullname,
+        week_start,
+        weekly_pageviews
+      ) %>%
+      group_by(fullname) %>%
+      summarise(
+        search_week_start = min(week_start),
+        search_week_end = max(week_start),
+        min_weekly_pageviews = min(weekly_pageviews),
+        median_weekly_pageviews = median(weekly_pageviews),
+        mean_weekly_pageviews = round(mean(weekly_pageviews), 2),
+        max_weekly_pageviews = max(weekly_pageviews),
+        stdin_weekly_pageviews = round(sd(weekly_pageviews), 2),
+        total_pageviews = sum(weekly_pageviews)
+      )
+    # set custome features for data table output
+    datatable(wikipedia_summary_df,
+      options = list(
+        paging = T, scrollX = T, searching = T, bInfo = T,
+        autoWidth = F,
+        class = "cell-border hover compact",
+        pageLength = 10,
+        initComplete = JS(
+          "function(settings, json) {",
+          "$(this.api().table().header()).css({'background-color': '#000', 'color': '#fff'});",
+          "}"
+        )
+      ),
+      escape = F
+    )
+  })
+
+  ########################################################
+  # Celebrity Hype Trends bar plot - average hype score #
+  ######################################################
+  
+  output$celebritytrendbarplot <- renderPlotly({
+    # initialize updated data for bar plot output
+    rescaled_wikipedia_df <- session_data_input()
+    aggregated_rescaled_wikipedia_df <- rescaled_wikipedia_df[, c(1, 5)]
+    aggregated_rescaled_wikipedia_df <- aggregate(aggregated_rescaled_wikipedia_df, by = list(aggregated_rescaled_wikipedia_df$fullname), FUN = mean)
+    aggregated_rescaled_wikipedia_df <- aggregated_rescaled_wikipedia_df[, c(1, 3)]
+    colnames(aggregated_rescaled_wikipedia_df) <- c("fullname", "avg_hype_score")
+    aggregated_rescaled_wikipedia_df$avg_hype_score <- round(aggregated_rescaled_wikipedia_df$avg_hype_score, 0)
+    ggplot(data = aggregated_rescaled_wikipedia_df, aes(x = fullname, y = avg_hype_score, colour = fullname)) +
+      geom_bar(stat = "identity", fill = "white", group = aggregated_rescaled_wikipedia_df$fullname) +
+      xlab("Average Hype Score") +
+      ylab(" ") +
+      ylim(0, 100) +
+      scale_color_brewer(palette = "Dark2") +
+      theme_classic() +
+      theme(
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.line.y = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        panel.background = element_rect(fill = "transparent"),
+        legend.position = "none"
+      ) -> P
+    plotly::ggplotly(P, height = 480.2, tooltip = c("x", "y")) %>% layout(dragmode = "select", paper_bgcolor = "transparent")
+  })
+
+  ######################################################
+  # Celebrity Hype Trends line plot  - hype over time #
+  ####################################################
+  
+  output$celebritytrendplot <- renderPlotly({
+    # initialize updated data for trend plot
+    rescaled_wikipedia_df <- session_data_input()
+    ggplot(data = rescaled_wikipedia_df, aes(x = week_start, y = hype_score, colour = fullname)) +
+      geom_line(group = rescaled_wikipedia_df$fullname) +
+      geom_point(aes(color = fullname), size = 2) +
+      xlab(" ") +
+      ylab(" ") +
+      ggtitle("Trend Over Time") +
+      ylim(0, 100) +
+      scale_color_brewer(palette = "Dark2") +
+      theme_classic() +
+      theme(
+        axis.text.x = element_text(angle = 45, hjust = 1, size = 8),
+        panel.background = element_rect(fill = "transparent"),
+        legend.title = element_blank(),
+        legend.background = element_rect(fill = "transparent")
+      ) -> P
+    plotly::ggplotly(P, height = 500, tooltip = c("x", "y")) %>% layout(dragmode = "select", paper_bgcolor = "transparent")
+  })
+
+  ####################################
+  # Download handler - pageview raw #
+  ##################################
+  
+  output$download <- downloadHandler(
+    filename = function() {
+      paste0("wikipedia_df_", Sys.Date(), ".csv")
+    },
+    content = function(file) {
+      data <- (session_data_input() %>%
+        select("fullname", "week_start", "weekly_pageviews") %>%
+        arrange(desc(fullname)))
+      data$fullname <- gsub("_", " ", data$fullname)
+
+      write.csv(data,
+        file,
+        row.names = F
+      )
+    }
+  )
+
+  ##############################################
+  # Download handler - pageview summary stats #
+  ############################################
+  
+  output$downloadsummary <- downloadHandler(
+    filename = function() {
+      paste0("wikipedia_df_summary_stats_", Sys.Date(), ".csv")
+    },
+    content = function(file) {
+      data <- session_data_input() %>%
+        select(
+          fullname,
+          week_start,
+          weekly_pageviews
+        ) %>%
+        group_by(fullname) %>%
+        summarise(
+          search_start_date = min(week_start),
+          search_end_date = max(week_start),
+          min_weekly_pageviews = min(weekly_pageviews),
+          median_weekly_pageviews = median(weekly_pageviews),
+          mean_weekly_pageviews = round(mean(weekly_pageviews), 2),
+          max_weekly_pageviews = max(weekly_pageviews),
+          stdin_weekly_pageviews = round(sd(weekly_pageviews), 2),
+          total_weekly_pageviews = sum(weekly_pageviews)
+        )
+
+      write.csv(data,
+        file,
+        row.names = F
+      )
+    }
+  )
+  
+  #################################################################################################
+  #                                       REACTIVE DATA UPDATE ELEMENTS                          #
+  ###############################################################################################
+  
+  # Reactive expressions are smarter than regular R functions.
+  # They cache their values and know when their values have become outdated.
+  session_data_input <- reactive({
+    # initialize global server wikipedia data frame
+    wikipedia_df <- source_wikipedia_data(celebrities = req(input$dropdownselectceleb), date_range = input$daterangetrenddate, input$platforminput)
+    wikipedia_df$date <- as.Date(wikipedia_df$date, format = "%Y-%m-%d")
+    wikipedia_df$fullname <- gsub("_", " ", wikipedia_df$fullname)
+    groupped_wikipedia_df <- group_wikipedia_df(wikipedia_df)
+    rescaled_wikipedia_df <- generate_trend_scores(groupped_wikipedia_df)
+    return(rescaled_wikipedia_df)
+  })
+}
---
+++ ui.R
@@ -0,0 +1,194 @@
+source("source_wikipedia_data.R")
+
+ui <- dashboardPage(
+  
+  skin = "black",
+  dashboardHeader(
+    title = "Hype Dashboard",
+    tags$li(
+      class = "dropdown",
+      tags$style(".main-header {max-height: 50px;}"),
+      tags$style(".main-header .logo {height: 55px;padding-top: 5px !important;}"),
+      tags$style(".sidebar-toggle {height: 55px; padding-top: 20px !important;}"),
+      tags$style(".navbar {min-height:45px !important;}")
+    ),
+    tags$li(class = "dropdown", tags$a(class = "btn", href = "https://tigranmelkonian.github.io/webPage/", tags$img(src = "globe.png", title = "Personal web page!", height = 21, width = 21))),
+    tags$li(class = "dropdown", tags$a(class = "btn", href = "https://github.com/TigranMelkonian", tags$img(src = "github.png", title = "GitHub", height = 17, width = 17)))
+  ),
+  dashboardSidebar(
+    sidebarMenu(
+      id = "sidebar", # id important for updateTabItems
+      menuItem("Home", tabName = "home", icon = icon("home", lib = "font-awesome", "fa-1x")),
+      menuItem("Celebrity Hype", tabName = "celebrityhypetrends", icon = icon("chart-line", lib = "font-awesome", "fa-1x")),
+      menuItem("Wikipedia Page Views", tabName = "celebritywikipediapageviews", icon = icon("table", lib = "font-awesome", "fa-1x"))
+    )
+  ),
+  dashboardBody(
+    
+    #################################################################################################
+    #                                       Main Dashbaord Body                                    #
+    ###############################################################################################
+    
+    tabItems(
+      
+      ###############################
+      # Home - dashboard info text #
+      #############################
+      
+      tabItem(
+        "home",
+        withTags({
+          div(
+            class = "header", checked = NA, style = "text-align:center;",
+            h1("Welcome to the Celebrity Hype Shiny Dashboard!"),
+            a(img(class = "img-circle", src = "trend_logo.png", alt = "User Avatar"))
+          )
+        }),
+        fluidRow(
+          column(
+            width = 4, align = "left",
+            withTags({
+              div(
+                class = "body", checked = NA,
+                h3("Celebrity Hype ", style = "font-weight: bold;text-align:center", a(img(class = "img-square", src = "chart-line-solid.png"))),
+                p("The Celebrity Hype tab contains a trends search feature that produces a graphical representation of the relative scale of public interest (hype..) for your favorite celebrities! The tool allows for the tracking of various celebrities and search terms that have a verified Wikipedia page."),
+                p("The primary goal of this feature is to help the user understand how much hype is behind specific celebrity life events. For example: if news breaks involving Lori Loughlin and a college admissions scandal, you would be able to understand how much hype it resulted in and how long it lasted. Furthermore you could compare the resulting hype of Lori's admission scandal against Felicity Hufman's college admission scandal to draw indirect insight into how impactful an event was given relative context."),
+                p("You are encouraged to play around with custom search queries, data ranges, and platform designations to view whether a celebrity is on the rise or declining across all user platforms (desktop, mobile-app,and mobile-web)."),
+                p("If you aren't able to find a celebrity in the preloaded celebrity list, simply add the query to the celebrity table by typing in your desired celebrity and selecting the top search result ('Add Query...')."),
+                p("Though it is possible to add any query to the celebrity table, it is recommended that you only add celebrity names and no other search terms to ensure propper functionality of the tool."),
+                p("However, if you would like to mix in custom search queries that are not celebrity names, it is reconmended that you either vizualize the hype trend by it's self or compare among like search types (i.e. celebrities with celebrities, companies with companies, trend topics with trend topics, etc.). This will ensure that you are searching across simillar search volume scales and that no search term is unintentionally washed out or blown out of proportion.")
+              )
+            })
+          ),
+          column(
+            width = 4, align = "left",
+            withTags({
+              div(
+                class = "body", checked = NA,
+                h3("Wikipedia Page Views ", style = "font-weight: bold;text-align:center;", a(img(class = "img-square", src = "table-solid.png"))),
+                p("The Wikipedia Page Views tab contains raw downloadable Wikipedia page view data-tables for the celebrities and date-range you specified on the Celebrity Hype tab"),
+                p("Wikipedia page views refers to the number of times a particular Wikipedia page has been requested. Using the R package 'pageviews', it is possible to see statistics on how often any Wikipedia page has been viewed during various times, including data as far back as July 1, 2015. These figures do not reflect the number of unique visitors a page has received."),
+                p("The statistics do not consider how long people have stayed at the article. Whether they load the article and read from begin to end, or if they leave it in seconds, it will count as one view."),
+                p("It is important to note that in some rare cases pageviews might have been purposely manipulated by the use of scripts or malware."),
+                p("For more information about Wikipedia Page View Data, please refer to this", a(href = "https://en.wikipedia.org/wiki/Wikipedia:Pageview_statistics", "link."))
+              )
+            })
+          ),
+          column(
+            width = 4, align = "left",
+            withTags({
+              div(
+                class = "body", checked = NA,
+                h3("Methodology ", style = "font-weight: bold;text-align:center;", a(img(class = "img-square", src = "flask-solid.png"))),
+                p("Raw daily wikipedia pageview data is transformed to mimic the Google Trends score format! For more information on how Google generates its trend score for any given queery, please refer to this", a(href = "https://support.google.com/trends/answer/4365533?hl=en", "link.")),
+                p(
+                  h4("Celebrity Hype Score Standardization Process", style = "font-weight: bold;"),
+                  h5("Search Window:", style = "font-weight: bold;"),
+                  p("The script takes into account the users designated search window to generate relative hype scores. Generally, for comparative analyses, large window size  help flush out insignificant spikes in the genereated weekly hype scores and highlight true variations in hype for a two or more celebrities (search term) over time"),
+                  h5("Score calculation:", style = "font-weight: bold;"),
+                  p("In creating the incident score, the daily pageview data is first grouped by total weekly pageview volume and divided by the total pageview volume of the trailing search window designated by the user. These ratios are finally rescaled on a range of 0-100 based on a week's respoective pageview volume proportion to the total pageview volume in the designated trailing window.")
+                )
+              )
+            })
+          )
+        )
+      ),
+      
+      ##########################
+      # Celebrity Hype Trends #
+      ########################
+      
+      tabItem(
+        "celebrityhypetrends",
+        titlePanel(div("Celebrity Hype Trends", style = "font-size: 32px; padding-bottom: 20px")),
+        fluidRow(
+          column(
+            4,
+            selectizeInput(
+              "dropdownselectceleb",
+              "Select Celebrity",
+              choices = sort(unique(as.character(celebrity_table$fullname))),
+              selected = c("Kim Kardashian West", "Kanye West"),
+              options = list(create = TRUE),
+              multiple = T
+            )
+          ),
+          column(
+            4,
+            dateRangeInput(
+              "daterangetrenddate",
+              "Select Date Range",
+              start = as.Date(format(Sys.Date(), "%Y-%m-%d")) - 730,
+              end = format((Sys.Date() - 1), "%Y-%m-%d"),
+              min = format(as.Date("2016-07-01"), "%Y-%m-%d"),
+              max = as.Date(format(Sys.Date(), "%Y-%m-%d")) - 1
+            ),
+            tags$style(HTML(".datepicker {z-index:99999 !important;}"))
+          ),
+          column(
+            4,
+            selectInput(
+              "platforminput",
+              "Select platform",
+              choices = c("all", "desktop", "mobile-app", "mobile-web"),
+              selected = c("all"),
+              multiple = F
+            )
+          ),
+          fluidRow(column(8, plotlyOutput("celebritytrendplot", height = "100%")), column(4, plotlyOutput("celebritytrendbarplot", height = "100%")))
+        )
+      ),
+      
+      ####################################
+      # pageview data tables / download #
+      ###################################
+      
+      tabItem(
+        "celebritywikipediapageviews",
+        fluidRow(
+          column(
+            10,
+            withTags({
+              div(
+                class = "header", checked = NA, style = "text-align:center;",
+                h2("Page View Summary Statistics")
+              )
+            }),
+            div(dataTableOutput("summarystats"),
+              style = "font-size: 85%; width: 100%;"
+            )
+          ),
+          column(
+            2,
+            fluidRow(
+              style = "padding-top:61px;text-align:left;",
+              downloadButton("downloadsummary", "Download")
+            )
+          )
+        ),
+        fluidRow(
+          column(
+            10,
+            withTags({
+              div(
+                class = "header", checked = NA, style = "text-align:center; padding-top: 40px",
+                h2("Raw Wikipedi Page Views")
+              )
+            }),
+            div(dataTableOutput("wikipedia_df_raw"),
+              style = "font-size: 85%; width: 100%;"
+            )
+          ),
+          column(
+            2,
+
+            fluidRow(
+              style = "padding-top:101px;text-slign:left;",
+              downloadButton("download", "Download")
+            )
+          )
+        )
+      )
+    )
+  )
+)
